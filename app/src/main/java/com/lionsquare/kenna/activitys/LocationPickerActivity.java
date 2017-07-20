7d3beb088266a87898d4174aef0d7fd82c7372be
argaed <edgar.arana@operacionesdelnorte.com>
2017-07-20 10:22:12 -0500

    ajustaado maps de cooreenadas

--- app/src/main/java/com/lionsquare/kenna/activitys/LocationPickerActivity.java
+++ app/src/main/java/com/lionsquare/kenna/activitys/LocationPickerActivity.java
@@ -1,7 +1,15 @@
 package com.lionsquare.kenna.activitys;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,14 +26,32 @@ import com.lionsquare.kenna.databinding.ActivityLocationPickerBinding;
 public class LocationPickerActivity extends AppCompatActivity implements View.OnClickListener {
     ActivityLocationPickerBinding binding;
     private static final int PLACE_PICKER_REQUEST = 1;
+    private static final int PERMIS_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_location_picker);
         binding.placeSearchDialogOkTV.setOnClickListener(this);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            verifyPermission();
+        } else {
+            binding.placeSearchDialogOkTV.setEnabled(true);
+        }
+
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            verifyPermission();
+        } else {
+            binding.placeSearchDialogOkTV.setEnabled(true);
+        }
+
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -61,7 +87,64 @@ public class LocationPickerActivity extends AppCompatActivity implements View.On
 
     @Override
     public void onClick(View v) {
-        locationPlacesIntent();
+        if (binding.placeSearchDialogOkTV.isEnabled())
+            locationPlacesIntent();
+        else
+            showSnackBar();
 
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void verifyPermission() {
+        int writePermission = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (writePermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermission();
+            binding.placeSearchDialogOkTV.setEnabled(false);
+        } else {
+            //saveComments();
+            binding.placeSearchDialogOkTV.setEnabled(true);
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            showSnackBar();
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMIS_LOCATION);
+        }
+    }
+
+    private void showSnackBar() {
+        Snackbar snackbar = Snackbar.make(binding.alpRoot, R.string.permission_location, Snackbar.LENGTH_INDEFINITE);
+        View snackBarView = snackbar.getView();
+        snackBarView.setBackgroundColor(getResources().getColor(R.color.primaryColor));
+        snackbar.setAction("Configurar", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", "com.lionsquare.kenna", null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        }).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMIS_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //saveComments();
+                binding.placeSearchDialogOkTV.setEnabled(true);
+            } else {
+                showSnackBar();
+                binding.placeSearchDialogOkTV.setEnabled(false);
+            }
+        }
+    }
+
 }
