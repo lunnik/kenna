f674aeacc7904e342ecd2d03a31dc770fcc9026c
lunnik <argaed@gmail.com>
2017-07-22 16:20:37 -0500

    rv

---
+++ app/src/main/java/com/lionsquare/kenna/activitys/WallPetActivity.java
@@ -0,0 +1,15 @@
+package com.lionsquare.kenna.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lionsquare.kenna.R;
+
+public class WallPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wall_pet);
+    }
+}
---
+++ app/src/main/java/com/lionsquare/kenna/holder/ViewHolderPet.java
@@ -0,0 +1,8 @@
+package com.lionsquare.kenna.holder;
+
+/**
+ * Created by edgararana on 22/07/17.
+ */
+
+public class ViewHolderPet {
+}
