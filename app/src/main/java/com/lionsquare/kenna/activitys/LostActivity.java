9f926cd01f8b34cb1ac0335e87b8461ab82ca3c4
argaed <edgar.arana@operacionesdelnorte.com>
2017-07-25 12:51:53 -0500

    lost

--- app/src/main/java/com/lionsquare/kenna/activitys/LostActivity.java
+++ app/src/main/java/com/lionsquare/kenna/activitys/LostActivity.java
@@ -4,20 +4,29 @@ package com.lionsquare.kenna.activitys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.lionsquare.kenna.R;
 
 
-public class LostActivity extends AppCompatActivity   {
+public class LostActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-
+    private GoogleMap googleMap;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost);
-
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+    }
 }
