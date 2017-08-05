d301ae5e30792292513510a360bbf07e5aa50899
lunnik <argaed@gmail.com>
2017-08-05 13:14:10 -0500

    lotties

--- app/src/main/java/com/lionsquare/comunidadkenna/activitys/MainActivity.java
+++ app/src/main/java/com/lionsquare/comunidadkenna/activitys/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,8 +21,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        finish();
+
+        LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+        animationView.setAnimation("location_pin.json");
+        animationView.loop(true);
+        animationView.playAnimation();
+       /* finish();
         Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
+        startActivity(i);*/
     }
 }
