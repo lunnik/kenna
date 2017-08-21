63f2545d27cb9b6e9dbd815f8996b05d2b856c35
lunnik <argaed@gmail.com>
2017-08-21 15:34:45 -0500

    se arrgelo lo de la notificsion

--- app/src/main/java/com/lionsquare/comunidadkenna/task/NotificationPetLost.java
+++ app/src/main/java/com/lionsquare/comunidadkenna/task/NotificationPetLost.java
@@ -1,9 +1,14 @@
 package com.lionsquare.comunidadkenna.task;
 
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.lionsquare.comunidadkenna.R;
@@ -24,26 +29,41 @@ public class NotificationPetLost {
 
 
     public void recivePet(String title, int id) {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
-                        .setSmallIcon(android.R.drawable.stat_sys_warning)
-                        .setLargeIcon((((BitmapDrawable) context.getResources()
-                                .getDrawable(R.mipmap.ic_launcher)).getBitmap()))
-                        .setContentTitle(title)
-                        .setContentText("Se perdio serca de donde estas")
-                        .setContentInfo("4")
-                        .setTicker("Se perdio serca de donde estas");
+
+        Intent intent = new Intent(context, DetailsLostActivity.class);
+        intent.putExtra("id", id);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //sonido
+        Uri sonido = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        //vibracion
+        long[] vibracion = new long[]{1000, 500, 100};
 
 
-        Intent notIntent =
-                new Intent(context, DetailsLostActivity.class);
-        notIntent.putExtra("id", id);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
 
-        PendingIntent contIntent =
-                PendingIntent.getActivity(
-                        context, id, notIntent, 0);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+        //builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.devolucion));
+        builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher));
+        builder.setContentTitle(title);
 
-        mBuilder.setContentIntent(contIntent);
+        builder.setContentText("Se perdio cerca de ti ");
+        //builder.setSubText(msj);
+        builder.setSound(sonido);
+        builder.setVibrate(vibracion);
+        //usar led de notificacion
+        builder.setLights(Color.WHITE, 1, 0);
+
+
+        //Enviar la notificacion
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(id, builder.build());
+
+
+
     }
 
 
